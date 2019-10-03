2d5be19727559ca2027124e76be0a1db38579a8e
fikriadriansa21 <fikriadriansa21@gmail.com>
2019-10-03 11:16:06 +0700

    Initial Commit

---
+++ piket-cl-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  piket-cl-ios
+//
+//  Created by Fikri Adriansa Yudha on 03/10/19.
+//  Copyright © 2019 Unikom Codelabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
