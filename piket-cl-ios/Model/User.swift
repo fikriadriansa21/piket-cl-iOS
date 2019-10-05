da699c9afe2ab6923110956ac99dd4d4e4ec9354
fikriadriansa21 <fikriadriansa21@gmail.com>
2019-10-05 20:10:49 +0700

    add navigation big title and tableviewcontroller

---
+++ piket-cl-ios/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  piket-cl-ios
+//
+//  Created by Fikri Adriansa Yudha on 05/10/19.
+//  Copyright © 2019 Unikom Codelabs. All rights reserved.
+//
+
+import UIKit
+
+struct User: Codable {
+    var nim: String
+    var nama: String
+    var password: String
+}
