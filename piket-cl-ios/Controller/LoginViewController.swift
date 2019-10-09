97709358a795ba4a94ef245c92ff276f92a2d6b5
fikriadriansa21 <fikriadriansa21@gmail.com>
2019-10-09 13:22:49 +0700

    commit to dev-fikri

--- piket-cl-ios/Controller/LoginViewController.swift
+++ piket-cl-ios/Controller/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class LoginViewController: UIViewController {
 
@@ -15,16 +17,6 @@ class LoginViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
