3619275198aa16cfc7d3d04244ce77859cf40548
fikriadriansa21 <fikriadriansa21@gmail.com>
2019-11-17 23:53:09 +0700

    fixing bridge networking between network layer and alamo fire using Moya, but login and get list piket isn't done

--- piket-cl-ios/Controller/LoginViewController.swift
+++ piket-cl-ios/Controller/LoginViewController.swift
@@ -23,32 +23,18 @@ class LoginViewController: UIViewController {
     var finalNimText = ""
     var responseText = ""
     var passwordText = ""
-    let api = APIManager()
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         labelNim.text = finalNimText
-//        labelKeteranganPassword.text = responseText
         
         self.hideKeyboardWhenTappedAround()
     }
     
     @IBAction func loginAction(_ sender: UIButton) {
-        let alert = UIAlertController(title: "Warning", message: "Password must be filled", preferredStyle: .actionSheet)
-
-        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
-            print("Silahkan isi Password-mu")
-        }))  
         passwordText = textFieldPassword.text!
-        
-            api.loginUser(nim: finalNimText, password: passwordText){(success) in
-                self.performSegue(withIdentifier: "sendPassword", sender: self)
-            }
-//            api.getListPiket(){(success) in
-//                print("list piket")
-//            }
-        
     }
     
 }
