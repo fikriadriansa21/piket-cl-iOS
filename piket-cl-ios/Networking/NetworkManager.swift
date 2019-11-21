6919bc579f63a396794fe4192a3f4878be081bd8
Nisdew <anisadewi310118@gmail.com>
2019-11-21 11:39:01 +0700

    add NetworkManager

---
+++ piket-cl-ios/Networking/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  piket-cl-ios
+//
+//  Created by Codelabs Division on 21/11/19.
+//  Copyright © 2019 Unikom Codelabs. All rights reserved.
+//
+
+import Foundation
