389231152d09ce9b60c1083e62a8791d74a11f8a
fikriadriansa21 <fikriadriansa21@gmail.com>
2019-12-12 07:59:01 +0700

    problem : can't divide data into 3 section

--- piket-cl-ios/Controller/ListPiketHariIni/ListPiketHariIniViewController.swift
+++ piket-cl-ios/Controller/ListPiketHariIni/ListPiketHariIniViewController.swift
@@ -20,8 +20,10 @@ enum LoadDataPiket: Int {
 class ListPiketHariIniViewController: UIViewController {    
     var networkManager = NetworkManager()
     var piket = [Piket]()
-    var sudahPiket = [SudahPiket]()
+    var sudahPiket = [SudahPiket?]()
     var belumPiketBulanan = [BelumPiket]()
+
+    var itemPiket = [[Any]]()
     let defToken = UserDefaults.standard
     var nimAuth:String = ""
     
@@ -36,38 +38,47 @@ class ListPiketHariIniViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadDataPiket()
+        
+        itemPiket = [
+            [piket],
+            [sudahPiket],
+            [belumPiketBulanan]
+        ]
     }
     
     func getDataPiket(dataPiket: [Piket]){
         self.piket = dataPiket
+        print(self.piket)
     }
     
-    func getDataSudah(dataPiket: [SudahPiket]){
+    func getDataSudah(dataPiket: [SudahPiket?]){
         self.sudahPiket = dataPiket
+        print(self.sudahPiket)
     }
     
     func getDataBelum(dataPiket: [BelumPiket]){
         self.belumPiketBulanan = dataPiket
+        print(self.belumPiketBulanan)
     }
     
     public func loadDataPiket(){
         networkManager.getListPiket(){(listPiket) in
-            if let data = listPiket{
-                self.getDataPiket(dataPiket: data)
+            if let dataHariIni = listPiket{
+                self.getDataPiket(dataPiket: dataHariIni)
                 self.tableView?.reloadData()
             }
         }
         
         networkManager.getSudahPiketHariIni{(listSudahPiket) in
-            if let data = listSudahPiket{
-                self.getDataSudah(dataPiket: data)
+            if let dataSudah = listSudahPiket{
+                self.getDataSudah(dataPiket: dataSudah)
                 self.tableView?.reloadData()
             }
         }
         
         networkManager.getBelumPiketBulanan{(belumPiketBulanan) in
-            if let data = belumPiketBulanan{
-                self.getDataBelum(dataPiket: data)
+            if let dataBelum = belumPiketBulanan{
+                self.getDataBelum(dataPiket: dataBelum)
                 self.tableView?.reloadData()
             }
         }
@@ -78,21 +89,27 @@ class ListPiketHariIniViewController: UIViewController {
 extension ListPiketHariIniViewController: UITableViewDataSource, UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.piket.count
+        if section == 0{
+            return self.piket.count
+        }else if section == 1 {
+            return self.sudahPiket.count
+        }else if section == 2 {
+            return self.belumPiketBulanan.count
+        }else{
+            return 0
+        }
+        
      }
     
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let label = UILabel()
-        label.backgroundColor = .lightGray
-            if section == 0 {
-                label.text = "Belum Piket"
-            }else if section == 1{
-                label.text = "Sudah Piket"
-            }else{
-                label.text = "Belum Piket Bulanan"
-            }
-        return label
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0: return "Piket Hari Ini"
+        case 1: return "Sudah Piket"
+        case 2: return "Belum Piket Bulan Ini"
+        default: return nil
+        }
     }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 3
     }
@@ -100,28 +117,40 @@ extension ListPiketHariIniViewController: UITableViewDataSource, UITableViewDele
      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
       let cell = tableView.dequeueReusableCell(withIdentifier: "cellJadwalPiket", for: indexPath) as! LisPiketTableViewCell
         
-        let eachPiket = piket[indexPath.row]
-        
-        if indexPath.section == LoadDataPiket.PiketHariIni.rawValue{
-            cell.backgroundColor = .green
-        }else if indexPath.section == LoadDataPiket.SudahPiketHariIni.rawValue{
-            cell.backgroundColor = .blue
-        }else if indexPath.section == LoadDataPiket.BelumPiketBulanan.rawValue {
-            cell.backgroundColor = .red
-        }else{
-            cell.backgroundColor = .white
-        }
-        
-        cell.labelNama?.text = eachPiket.nama_anggota
-        cell.labelJobPiket?.text = eachPiket.jenis_piket
-        
-        if(eachPiket.nim == nimAuth){
+        let piketData = piket[indexPath.row]
+        let sudah = sudahPiket[indexPath.row]
+        let belum = belumPiketBulanan[indexPath.row]
+
+        if(piketData.nim == nimAuth){
             print(defToken.string(forKey: "token") as Any)
-            cell.buttonSelesaiPiket.setTitle(eachPiket.status, for: .normal)
+            cell.buttonSelesaiPiket.setTitle(piketData.status, for: .normal)
             cell.buttonSelesaiPiket.isHidden = false
         }else{
             cell.buttonSelesaiPiket.isHidden = true
         }
+        
+        if indexPath.section == 0{
+            cell.backgroundColor = .green
+            cell.labelNama?.text = piketData.nama_anggota
+            cell.labelJobPiket?.text = piketData.jenis_piket
+        }else if indexPath.section == 1{
+            cell.backgroundColor = .blue
+            cell.labelNama?.text = sudah?.nama_anggota
+            cell.labelJobPiket?.text = sudah?.jenis_piket
+        }else if indexPath.section == 2{
+            cell.backgroundColor = .red
+            cell.labelNama?.text = belum.nama_anggota
+            cell.labelJobPiket?.text = belum.jenis_piket
+        }else{
+            cell.backgroundColor = .white
+        }
+                    
       return cell
      }
+}
+
+extension Collection where Indices.Iterator.Element == Index {
+    subscript (safe index: Index) -> Iterator.Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
 }
